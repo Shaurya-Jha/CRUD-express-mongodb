31d2f7001e6d7163bb86e89827ff9971e8d8c18e
Shaurya Jha <shauryajha1602@gmail.com>
2024-01-28 23:21:29 +0530

    working on basics of crud api

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        "message":"Hello CRUD Node Express"
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+})
